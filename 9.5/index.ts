57716fc1a76a4ac3cc396af1e875860c9b9c20f3
akuivan <akuivan@users.noreply.github.com>
2024-09-17 09:20:27 +0300

    Add exercise 9.5

---
+++ 9.5/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import { calculateBmi } from './calculateBmi';
+
+const app = express();
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (!req.query.height || !req.query.weight || isNaN(height) || isNaN(weight)) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const bmi = calculateBmi(height, weight);
+
+  return res.json({
+    weight,
+    height,
+    bmi
+  });
+});
+
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
