257512ebe634238ef4b98bb747c28e038bcca825
akuivan <akuivan@users.noreply.github.com>
2024-09-20 09:29:48 +0300

    Add exercise 9.10

---
+++ 9.10/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import diagnoseRouter from "./routes/diagnosesRoute";
+
+const app= express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3001; //this is the port in patientor's request url
+
+app.get('/api/ping', (_req, res) => {
+    console.log('someone pinged here');
+    res.send("pong");
+});
+
+app.use('/api/diagnoses', diagnoseRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
