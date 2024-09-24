ab5f4b0ba9dc6467189a86f1b30c9f42db20d2dc
akuivan <akuivan@users.noreply.github.com>
2024-09-24 15:00:06 +0300

    Add exercise 9.19

---
+++ 9.19/diary-frontend/src/App.tsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from 'react';
+import { DiaryEntry, NewDiaryEntry } from './types';
+import { getAllDiaryEntries, createDiaryEntry } from './diaryService';
+import axios from "axios";
+
+const App = () => {
+  const [diaryEntries, setDiaryEntries] = useState<DiaryEntry[]>([]);
+  // states for a new diary entry 
+  const [newComment, setNewComment] = useState('');
+  const [newDate, setNewDate] = useState('');
+  const [newWeather, setNewWeather] = useState('');
+  const [newVisibility, setNewVisibility] = useState('');
+  //state for possible error 
+  const [errorMessage, setErrorMessage]= useState('');
+
+  useEffect(() => {
+    getAllDiaryEntries().then(data => {
+      setDiaryEntries(data);
+    });
+  }, []);
+
+  const diaryEntryCreation = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    const newDiaryEntry: NewDiaryEntry = {
+      date: newDate,
+      weather: newWeather as any, 
+      visibility: newVisibility as any, 
+      comment: newComment,
+    };
+
+    try {
+      const data = await createDiaryEntry(newDiaryEntry);
+      setDiaryEntries(diaryEntries.concat(data));
+      
+      setNewComment('');
+      setNewDate('');
+      setNewWeather('');
+      setNewVisibility('');  
+      setErrorMessage('');
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+          setErrorMessage(error.response?.data);
+        } else {
+          console.error(error);
+        }
+    };
+  };
+
+  return (
+    <div>
+      <h2>Add a new entry</h2>
+      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
+      <form onSubmit={diaryEntryCreation}>
+        <div>
+          <label>Date:</label>
+          <input
+            value={newDate}
+            onChange={({ target }) => setNewDate(target.value)}
+          />
+        </div>
+        <div>
+          <label>Visibility:</label>
+          <input
+            value={newVisibility}
+            onChange={({ target }) => setNewVisibility(target.value)}
+          />
+        </div>
+        <div>
+          <label>Weather:</label>
+          <input
+            value={newWeather}
+            onChange={({ target }) => setNewWeather(target.value)}
+          />
+        </div>
+        <div>
+          <label>Comment:</label>
+          <input
+            value={newComment}
+            onChange={({ target }) => setNewComment(target.value)}
+          />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+
+      <h2>Diary entries</h2>
+      {diaryEntries.map(diaryEntry => (
+        <div key={diaryEntry.id}>
+          <h4>{diaryEntry.date}</h4>
+          <p>Comment: {diaryEntry.comment}</p>
+          <p>Visibility: {diaryEntry.visibility}</p>
+          <p>Weather: {diaryEntry.weather}</p>
+        </div>
+      ))}
+
+    </div>
+  );
+};
+
+export default App;
